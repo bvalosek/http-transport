67d7cb3f9e756c18a420e657141eb9d13ea86582
Brandon Valosek <bvalosek@gmail.com>
2014-09-15 11:34:59 -0500

    added tests

---
+++ test/type-checks.js
@@ -0,0 +1,28 @@
+var test = require('tape');
+
+var HttpTransport = require('../index.js');
+
+test('constructor checks', function(t) {
+  t.plan(4);
+  t.throws(function() { new HttpTransport(123); }, TypeError);
+  t.throws(function() { new HttpTransport(null); }, TypeError);
+  t.throws(function() { new HttpTransport([]); }, TypeError);
+  t.throws(function() { new HttpTransport({}); }, TypeError);
+
+  // no throw
+  new HttpTransport();
+});
+
+test('method checks', function(t) {
+  t.plan(8);
+
+  t.throws(function() { new HttpTransport().get(123); }, TypeError);
+  t.throws(function() { new HttpTransport().post(123); }, TypeError);
+  t.throws(function() { new HttpTransport().put(123); }, TypeError);
+  t.throws(function() { new HttpTransport().del(123); }, TypeError);
+
+  t.throws(function() { new HttpTransport().get(); }, TypeError);
+  t.throws(function() { new HttpTransport().post(); }, TypeError);
+  t.throws(function() { new HttpTransport().put(); }, TypeError);
+  t.throws(function() { new HttpTransport().del(); }, TypeError);
+});
